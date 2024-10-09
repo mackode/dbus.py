e774af08194fb4aab3d6e041106523e2d3b61168
Bartek Makowski <biuro@mackode.pl>
2024-10-09 20:58:27 +0000

    Pause the players

--- search.sh
+++ search.sh
@@ -1,5 +1,5 @@
 #!/bin/zsh
 
-dbus-send --print-reply --dest=org.freedesktop.DBus / org.freedesktop.DBus.ListNames \
+dbus-send --print-reply --dest=org.freedesktop.DBus /org.freedesktop.DBus.ListNames \
   | grep -Eo 'org.mpris.MediaPlayer2.[^"]+' | nl -ba -s\: -w1
 
