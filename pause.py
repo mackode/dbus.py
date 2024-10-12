3bcaf197e749ff57455914a94b958363c0392d4a
Bartek Makowski <biuro@mackode.pl>
2024-10-12 07:49:09 +0000

    Add DBus audio pausing script

--- pause.py
+++ pause.py
@@ -2,6 +2,11 @@
 
 import dbus
 
+def pause_playback(bus, name):
+    player = bus.get_object(name, "/org/mpris/MediaPlayer2")
+    player_iface = dbus.Interface(player, dbus_interface="org.mpris.MediaPlayer2.Player")
+    player_iface.Pause()
+
 bus = dbus.SessionBus()
 dbus_daemon = bus.get_object("org.freedesktop.DBus", "/")
 dbus_iface = dbus.Interface(dbus_daemon, dbus_interface="org.freedesktop.DBus")
@@ -9,5 +14,5 @@ dbus_iface = dbus.Interface(dbus_daemon, dbus_interface="org.freedesktop.DBus")
 names = dbus_iface.ListNames()
 for name in names:
     if name.startswith("org.mpris.MediaPlayer2"):
-        print(name)
+        pause_playback(bus, name)
 
